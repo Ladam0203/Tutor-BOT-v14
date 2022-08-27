03668ba1d636da5f09ac872c4c6f9584457aa0c2
Ádám Lőrincz <adam7605@easv365.dk>
2022-08-27 13:19:11 +0200

    Basic use of embed messages
    So they are easier to tell apart from non-bot messages

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ REFACTORING!!!
 */
 
 // Require the necessary discord.js classes
-const { Client, GatewayIntentBits, ChannelType, PermissionsBitField, InteractionCollector } = require('discord.js');
+const { Client, GatewayIntentBits, ChannelType, PermissionsBitField, InteractionCollector, EmbedBuilder } = require('discord.js');
 const { token } = require('./config.json');
 
 // Create a new client instance
@@ -37,7 +37,7 @@ client.on('interactionCreate', async interaction => {
 
 			if (interaction.channel.name !== "open-a-ticket")
 			{
-				await interaction.reply({ content: "This command cannot be used here!", ephemeral: true });
+				await interaction.reply(asEmbed("You cannot use this command here!", true));
 				return;
 			}
 
@@ -63,15 +63,15 @@ client.on('interactionCreate', async interaction => {
 
 			let ticketChannel = client.channels.cache.find(c => c.name === ticketChannelName)
 
-			await interaction.reply({ content: "Your ticket has been created in: <#" + ticketChannel.id + ">.", ephemeral: true });
+			await interaction.reply(asEmbed("Your ticket has been created in: <#" + ticketChannel.id + ">.", true));
 
 			//change message if tutor or tutors are picked
-			await ticketChannel.send({ content: "Hey, <@" + interaction.user.id + ">! Please elaborate on your question while we find a tutor to assist you!", ephemeral: true });
+			await ticketChannel.send(asEmbed("Hey, <@" + interaction.user.id + ">! Please elaborate on your question while we find a tutor to assist you!", false));
 
 			const tutorRole = interaction.guild.roles.cache.find(r => r.name === 'Tutor');
 			const ticketOpenedPingChannel = client.channels.cache.find(c => c.name === "ticket-opened-ping");
 
-			await ticketOpenedPingChannel.send("<@&" + tutorRole.id+">s! <@" + interaction.user.id + "> needs assistance in <#" + ticketChannel.id + ">!");
+			await ticketOpenedPingChannel.send(asEmbed("<@&" + tutorRole.id+">s! <@" + interaction.user.id + "> needs assistance in <#" + ticketChannel.id + ">!"), false);
 		}
 
 		if (interaction.options.getSubcommand() === 'claim') {
@@ -80,25 +80,25 @@ client.on('interactionCreate', async interaction => {
 			//check permission (Tutor role)
 			if (!interaction.member.roles.cache.some(role => role.name === "Tutor"))
 			{
-				await interaction.reply({ content: "Insufficient permissions!", ephemeral: true });
+				await interaction.reply(asEmbed("Insufficient permissions!", true));
 				return;
 			}
 			//check channel
 			if (!interaction.channel.name.match("^ticket-[a-z0-9]{4}") || interaction.channel.parent.name !== "Open Tickets") { //TODO: write a regex 
-				await interaction.reply({ content: "This command cannot be used here!", ephemeral: true });
+				await interaction.reply(asEmbed("You cannot use this command here!", true));
 				return;
 			}
 			//move ticket to ongoing
 			let ongoingTicketsCategory = client.channels.cache.find(c => c.name === "Ongoing Tickets")
 			interaction.channel.setParent(ongoingTicketsCategory)
 			//announce who came to help
-			await interaction.reply("<@" + interaction.user.id + "> is here to help!");
+			await interaction.reply(asEmbed("<@" + interaction.user.id + "> is here to help!", false));
 		}
 
 		if (interaction.options.getSubcommand() === 'close') {
 			//check channel
 			if ((!interaction.channel.name.includes("ticket") || interaction.channel.name === "ticket-opened-ping") || interaction.channel.parent.name !== "Ongoing Tickets") { //TODO: write a regex 
-				await interaction.reply({ content: "This command cannot be used here!", ephemeral: true });
+				await interaction.reply(asEmbed("You cannot use this command here!", true));
 				return;
 			}
 
@@ -113,7 +113,7 @@ client.on('interactionCreate', async interaction => {
 			interaction.channel.permissionOverwrites.create(interaction.channel.guild.roles.everyone, { SendMessages: false });
 
 			//announce who came to help
-			await interaction.reply("Ticket has been closed and it can be found under closed tickets, WARNING: closed tickets will be deleted after a set amount of time!");
+			await interaction.reply(asEmbed("Ticket has been closed and it can be found under closed tickets, WARNING: closed tickets will be deleted after a set amount of time!", false));
 		}
 	}
 });
@@ -142,4 +142,12 @@ function deleteChannelsInCategory(client, category) {
 
 function findChannel(client, channelName, type) {
 	return client.channels.cache.find(channel => channel.name === channelName && channel.type === type);
+}
+
+function asEmbed(message, isEphemeral) {
+	let embed = new EmbedBuilder()
+	.setColor(0x00CED1)
+	.setDescription(message);
+
+	return {embeds: [embed], ephemeral: isEphemeral };
 }
