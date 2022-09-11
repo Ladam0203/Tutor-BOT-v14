a56440f2058428eaca43040648f66705d4918ca8
Ádám Lőrincz <adam7605@easv365.dk>
2022-09-11 13:25:07 +0200

    Server API setup for get and pull

--- Status REST API/server_api.js
+++ Status REST API/server_api.js
@@ -2,12 +2,22 @@ const express = require('express')
 const app = express()
 const port = 80
 
+const data = {
+  status: "Offline"
+}
+
+app.post('/', function(request, response){
+  data = JSON.parse(request.body);      // your JSON
+   //response.send(request.body);    // echo the result back
+   console.log("Status object received.")
+});
+
 app.get('/', (req, res) => {
   //res.header("Content-Type",'application/json');
-  res.send("Online");
-  console.log("Staus requested")
+  res.send(data);
+  console.log("Status object sent.")
 })
 
 app.listen(port, () => {
   console.log(`WomBot Status API listening on port ${port}`)
-})
+}) //accasd
