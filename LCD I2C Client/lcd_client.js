0c9e13d520b2b9a41e1b99c8a5f8d758a8bd1d67
Ádám Lőrincz <adam7605@easv365.dk>
2022-09-11 13:13:18 +0200

    Modified LCD and API server files according to prev dev on Pi

--- LCD I2C Client/lcd_client.js
+++ LCD I2C Client/lcd_client.js
@@ -5,6 +5,14 @@ const LCD = require('raspberrypi-liquid-crystal');
 const lcd = new LCD( 1, 0x27, 16, 2 );
 lcd.beginSync();
 
+const options = {
+    protocol: 'http:',
+    hostname: 'http://localhost',
+    port: 80,
+    path: '/',
+    method: 'POST',
+};
+
 clock = 0;
 lcd.printLineSync(0, 'Uptime:');
 lcd.printLineSync(1, clock);
@@ -13,18 +21,33 @@ setInterval(updateClock, 1000);
 
 function updateClock() {
     http.get(url, res => {
-        let data = "";
+        let data = clock;
         res.on('data', chunk => {
           data += chunk;
         });
         res.on('end', () => {
-          if (data = "Online") {
-            clock++;
-            lcd.printLineSync(0, 'Uptime:');
-            lcd.printLineSync(1, new Date(clock * 1000).toISOString().slice(11,19));
-          }
+          console.log("Status received: " + clock);
         })
       }).on('error', err => {
         console.log(err.message);
       }).end();
+
+	const data = clock;
+	const req = http.request(options, (res) => {
+	    let data = clock;
+
+	    res.on('data', (chunk) => {
+		data += chunk;
+	    });
+
+	    res.on('end', () => {
+		console.log("Status sent: " + clock);
+	    });
+
+	}).on("error", (err) => {
+	    console.log("Error: ", err.message);
+	});
+
+	req.write(data);
+	req.end();
 }
